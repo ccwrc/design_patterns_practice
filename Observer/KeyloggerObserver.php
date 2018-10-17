83385a2bf8f3769fe25247514037b7991839696c
ccwrc <ccwrcltd@gmail.com>
2018-10-17 19:49:15 +0200

    backup

--- Observer/KeyloggerObserver.php
+++ Observer/KeyloggerObserver.php
@@ -4,7 +4,22 @@ declare(strict_types=1);
 
 namespace Patterns\Observer;
 
-class KeyloggerObserver
+use SplSubject;
+
+class KeyloggerObserver implements \SplObserver
 {
 
+    /**
+     * Receive update from subject
+     * @link https://php.net/manual/en/splobserver.update.php
+     * @param SplSubject $subject <p>
+     * The <b>SplSubject</b> notifying the observer of an update.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function update(SplSubject $subject)
+    {
+        // TODO: Implement update() method.
+    }
 }
--- Observer/PasswordManager.php
+++ Observer/PasswordManager.php
@@ -4,7 +4,42 @@ declare(strict_types=1);
 
 namespace Patterns\Observer;
 
-class PasswordManager
+class PasswordManager implements \SplSubject
 {
+    /**
+     * @var \SplObjectStorage
+     * @link https://devenv.pl/php-spl-class-splobjectstorage/
+     */
+    private $observers;
 
+    public function __construct()
+    {
+        $this->observers = new \SplObjectStorage();
+    }
+
+    /**
+     * Attach an SplObserver
+     * @link https://php.net/manual/en/splsubject.attach.php
+     */
+    public function attach(\SplObserver $observer): void
+    {
+        $this->observers->attach($observer);
+    }
+
+    /**
+     * Detach an observer
+     */
+    public function detach(\SplObserver $observer): void
+    {
+        $this->observers->detach($observer);
+    }
+
+    /**
+     * Notify an observer
+     * @link https://php.net/manual/en/splsubject.notify.php
+     */
+    public function notify(): void
+    {
+        // TODO: Implement notify() method.
+    }
 }
