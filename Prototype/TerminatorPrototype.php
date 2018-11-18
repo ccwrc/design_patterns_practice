7b3dc7b37308d15a777e1dd31f0af295a07172a3
ccwrc <ccwrcltd@gmail.com>
2018-11-18 18:03:21 +0100

    ramsey uuid added

--- Prototype/TerminatorPrototype.php
+++ Prototype/TerminatorPrototype.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace Patterns\Prototype;
 
+use Ramsey\Uuid\Uuid;
+
 abstract class TerminatorPrototype implements Location
 {
     /**
      * @var int
      */
     protected $location;
+
+    /**
+     * @var null|Uuid
+     */
+    protected $serialNumber;
 
     public function __construct(int $locationOfTeleportation)
     {
@@ -21,4 +28,22 @@ abstract class TerminatorPrototype implements Location
      * @return bool
      */
     abstract public function tryKillSarah(Sarah $sarah): bool;
+
+    public function setSerialNumber(): bool
+    {
+        try {
+            $this->serialNumber = Uuid::uuid4();
+            return true;
+        } catch (\Throwable $throwable) {
+            return false;
+        }
+    }
+
+    /**
+     * @return null|Uuid
+     */
+    public function getSerialNumber(): ?string
+    {
+        return $this->serialNumber->toString();
+    }
 }
