3262a414231c67625c2ee1132024ca6aa3832a68
ccwrc <ccwrcltd@gmail.com>
2018-12-13 18:12:50 +0100

    catchCriminals - added splat operator

--- AbstractFactory/ConcreteFactory/SpiderManFactory.php
+++ AbstractFactory/ConcreteFactory/SpiderManFactory.php
@@ -16,4 +16,24 @@ final class SpiderManFactory extends SpiderFactory
     {
         return new SpiderManCobweb($strength);
     }
+
+    /**
+     * @link http://php.net/manual/en/migration56.new-features.php splat operator
+     * @link http://itcraftsman.pl/co-nowego-w-php-5-6/ splat operator examples
+     * @param string ...$criminals
+     * @return string[]
+     */
+    public function catchCriminals(string ...$criminals): array
+    {
+        $caughtCriminals = [];
+
+        foreach ($criminals as $criminal) {
+            $isCaught = (bool)\rand(0, 1);
+            if ($isCaught) {
+                $caughtCriminals[] = $criminal;
+            }
+        }
+
+        return $caughtCriminals;
+    }
 }
