cab9b1dafd1b80e13ed8983b8bc3d29e7dffad24
ccwrc <ccwrcltd@gmail.com>
2019-01-15 17:04:08 +0100

    new tests: Cobweb + SpiderFactory

---
+++ tests/unit/AbstractFactory/CobwebTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\tests\unit\AbstractFactory;
+
+use Patterns\AbstractFactory\Cobweb;
+
+use PHPUnit\Framework\TestCase;
+
+class CobwebTest extends TestCase
+{
+    public function testCreate(): void
+    {
+        $cobweb = new class(69) extends Cobweb
+        {
+        };
+
+        $this->assertInstanceOf(Cobweb::class, $cobweb);
+    }
+}
---
+++ tests/unit/AbstractFactory/SpiderFactoryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\tests\unit\AbstractFactory;
+
+use Patterns\AbstractFactory\{Cobweb, SpiderFactory};
+
+use PHPUnit\Framework\TestCase;
+
+class SpiderFactoryTest extends TestCase
+{
+    public function testCreate(): void
+    {
+        $object = new class('Spider') extends SpiderFactory
+        {
+            /**
+             * @param int $strength
+             * @return Cobweb
+             */
+            public function makeWeb(int $strength): Cobweb
+            {
+                $cobweb = new class($strength) extends Cobweb
+                {
+                };
+                return $cobweb;
+            }
+        };
+
+        $this->assertInstanceOf(SpiderFactory::class, $object);
+    }
+}
