7d91a236637c4bcf19fc648f555b94ac0f8e12a9
ccwrc <ccwrcltd@gmail.com>
2019-03-10 17:55:13 +0100

    Simple Factory - done

---
+++ tests/unit/SimpleFactory/CatFactoryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\tests\unit\SimpleFactory;
+
+use Patterns\SimpleFactory\{Cat, CatFactory};
+
+use PHPUnit\Framework\TestCase;
+
+class CatFactoryTest extends TestCase
+{
+    public function testCreateCat(): void
+    {
+        $cat = CatFactory::createCat(true, true);
+
+        $this->assertInstanceOf(Cat::class, $cat);
+    }
+
+    public function testCreateFluffyCat(): void
+    {
+        $fluffyCat = CatFactory::createFluffyCat(false);
+
+        $this->assertInstanceOf(Cat::class, $fluffyCat);
+        $this->assertTrue($fluffyCat->isFluffy());
+    }
+}
