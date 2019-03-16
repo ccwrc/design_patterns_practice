6901ce6ec0b8678d0f72b91aeb82b308a4e29199
ccwrc <ccwrcltd@gmail.com>
2019-03-16 15:21:50 +0100

    AirConditioning - bug fix

--- Facade/AirConditioning.php
+++ Facade/AirConditioning.php
@@ -6,6 +6,8 @@ namespace Patterns\Facade;
 
 final class AirConditioning implements AirConditioningInterface
 {
+    public const MIN_TEMPERATURE = -5;
+    public const MAX_TEMPERATURE = 25;
     /**
      * @var bool
      */
@@ -18,7 +20,7 @@ final class AirConditioning implements AirConditioningInterface
     public function __construct(int $temperature)
     {
         $this->isWorking = false;
-        $this->temperature = $temperature;
+        $this->temperature = self::getCorrectTemperature($temperature);
     }
 
     /**
@@ -45,19 +47,22 @@ final class AirConditioning implements AirConditioningInterface
      */
     public function setTemperature(int $temperature): void
     {
-        if ($temperature < -5) {
-            $this->temperature = -5;
-            return;
-        }
-        if ($temperature > 25) {
-            $this->temperature = 25;
-            return;
-        }
-        $this->temperature = $temperature;
+        $this->temperature = self::getCorrectTemperature($temperature);
     }
 
     public function getTemperature(): int
     {
         return $this->temperature;
+    }
+
+    private static function getCorrectTemperature(int $temperature): int
+    {
+        if ($temperature < self::MIN_TEMPERATURE) {
+            return self::MIN_TEMPERATURE;
+        }
+        if ($temperature > self::MAX_TEMPERATURE) {
+            return self::MAX_TEMPERATURE;
+        }
+        return $temperature;
     }
 }
