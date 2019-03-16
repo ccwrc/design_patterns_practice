b26eeec788fd18339d455b15e9212698511a9763
ccwrc <ccwrcltd@gmail.com>
2019-03-16 17:46:43 +0100

    backup

--- tests/unit/Facade/Implementation/FullRelaxationEncoreTest.php
+++ tests/unit/Facade/Implementation/FullRelaxationEncoreTest.php
@@ -4,7 +4,21 @@ declare(strict_types=1);
 
 namespace Patterns\tests\unit\Facade\Implementation;
 
-class FullRelaxationEncoreTest
-{
+use Patterns\Facade\Implementation\FullRelaxationEncore;
 
+use PHPUnit\Framework\TestCase;
+
+class FullRelaxationEncoreTest extends TestCase
+{
+    /**
+     * integration test
+     */
+    public function testMakeItHappen(): void
+    {
+        $fre = new FullRelaxationEncore();
+        $this->assertFalse($fre->hasItHappened());
+
+        $fre->makeItHappen(20);
+        $this->assertTrue($fre->hasItHappened());
+    }
 }
