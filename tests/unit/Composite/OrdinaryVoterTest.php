7d7b31d28b5807d5ca8c23af1b25c688328a5e21
ccwrc <ccwrcltd@gmail.com>
2019-04-27 15:18:37 +0200

    RichVoterTest

--- tests/unit/Composite/OrdinaryVoterTest.php
+++ tests/unit/Composite/OrdinaryVoterTest.php
@@ -4,9 +4,43 @@ declare(strict_types=1);
 
 namespace Patterns\tests\unit\Composite;
 
+use Patterns\Composite\{ElectionsPart, OrdinaryVoter};
+
 use PHPUnit\Framework\TestCase;
 
 class OrdinaryVoterTest extends TestCase
 {
+    /**
+     * @return OrdinaryVoter
+     */
+    public function testCreate(): OrdinaryVoter
+    {
+        $ordinaryVoter = new OrdinaryVoter('ordinary');
+
+        $this->assertInstanceOf(ElectionsPart::class, $ordinaryVoter);
+
+        return $ordinaryVoter;
+    }
+
+    /**
+     * @depends testCreate
+     * @param OrdinaryVoter $ordinaryVoter
+     */
+    public function testGetVotingPower(OrdinaryVoter $ordinaryVoter): void
+    {
+        $this->assertSame(1, $ordinaryVoter->getVotingPower());
+    }
+
+    /**
+     * @depends testCreate
+     * @param OrdinaryVoter $ordinaryVoter
+     */
+    public function testAddElectionsPartException(OrdinaryVoter $ordinaryVoter): void
+    {
+        $electionsPart = $this->createMock(ElectionsPart::class);
+
+        $this->expectExceptionMessage('Patterns\Composite\OrdinaryVoter is a leaf');
+        $ordinaryVoter->addElectionsPart($electionsPart);
+    }
 
 }
