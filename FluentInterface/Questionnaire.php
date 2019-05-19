05f61d0310e03b3992eaa646c3546dc29331a1b2
ccwrc <ccwrcltd@gmail.com>
2019-05-19 16:44:12 +0200

    Questionnaire added

--- FluentInterface/Questionnaire.php
+++ FluentInterface/Questionnaire.php
@@ -4,10 +4,58 @@ declare(strict_types=1);
 
 namespace Patterns\FluentInterface;
 
-class Questionnaire
+/**
+ * Fluent Interface implementation.
+ */
+final class Questionnaire
 {
-    public function __construct()
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var Question[]
+     */
+    private $questions = [];
+
+    /**
+     * Function allows chaining.
+     * @param string $title
+     * @return Questionnaire
+     */
+    public function setTitle(string $title): self
     {
+        $this->title = $title;
+
+        return $this;
     }
 
+    /**
+     * Function allows chaining.
+     * @param Question $question
+     * @return Questionnaire
+     */
+    public function addQuestion(Question $question): self
+    {
+        $this->questions[] = $question;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return Question[]
+     */
+    public function getQuestions(): array
+    {
+        return $this->questions;
+    }
 }
