3596c0b9eafb07f6dd0f26d1376618ffb6364099
ccwrc <ccwrcltd@gmail.com>
2019-06-22 15:22:09 +0200

    MagicMethods class added

--- ExperimentalFolder/readme.txt
+++ ExperimentalFolder/readme.txt
@@ -1,3 +1,5 @@
 
 Have fun with PHP with Dr. Victor Frankenstein.
 
+https://pl.wikipedia.org/wiki/Afera_grabarzy_z_Frankenstein
+
---
+++ tests/unit/ExperimentalFolder/MagicMethodsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\tests\unit\ExperimentalFolder;
+
+use PHPUnit\Framework\TestCase;
+
+class MagicMethodsTest extends TestCase
+{
+
+}
