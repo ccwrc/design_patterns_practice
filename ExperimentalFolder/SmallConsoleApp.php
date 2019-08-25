c00c944bd99de8f64fd4e322ec193c70fce1b09f
ccwrc <ccwrcltd@gmail.com>
2019-08-25 16:36:09 +0200

    small console app - docs update + fix

--- ExperimentalFolder/SmallConsoleApp.php
+++ ExperimentalFolder/SmallConsoleApp.php
@@ -6,16 +6,19 @@ namespace Patterns\ExperimentalFolder;
 
 /**
  * Small PHP app launched from the console.
- * Running the script from the current folder type in console (PHP interpreter required): php SmallConsoleApp.php
+ * Running the script from the current folder, type in console (PHP interpreter required): php SmallConsoleApp.php
  * Optional service selection from the command line (default: github): php SmallConsoleApp.php --service=service_name
  *
  * @link https://4programmers.net/PHP/Aplikacje_konsolowe_w_PHP convert app to executable version
  * @link https://developer.github.com/v3/git/refs/ API github docs
+ * @link https://www.kerneldev.com/2017/12/16/how-to-build-a-command-line-application-using-php/ How to
+ * build an app using the symfony component.
  */
 
-// getting parameters from command line
+// Getting parameters from command line.
+$defaultService = 'github';
 $optionsFromCommandLine = getopt('', ['service::']);
-$service = $optionsFromCommandLine['service'] ?? 'github';
+$service = $optionsFromCommandLine['service'] ?? $defaultService;
 
 echo 'Enter owner name: ';
 $lineOwner = getLineFromStandardInput();
@@ -25,17 +28,19 @@ $lineRepository = getLineFromStandardInput();
 
 echo 'Enter branch name: ';
 $lineBranch = getLineFromStandardInput();
-// end (getting parameters from command line)
+// End (Getting parameters from command line).
 
+// Main app core
 switch ($service) {
-    case 'github':
+    case $defaultService:
         echo getLastShaFromGithub($lineOwner, $lineRepository, $lineBranch);
         break;
     default:
-        echo 'Unknown service \'' . $service . '\'';
+        echo "\n Unknown service '" . $service . "'\n";
 }
+// End (Main app core).
 
-// more or less useful functions
+// More or less useful functions below.
 function getLineFromStandardInput(): string
 {
     $handle = fopen('php://stdin', 'r');
@@ -45,26 +50,26 @@ function getLineFromStandardInput(): string
     return $line;
 }
 
+/**
+ * @return string (SHA or error message)
+ */
 function getLastShaFromGithub(string $owner, string $repository, string $branch): string
 {
-    try {
-        $options = [
-            'http' => [
-                'method' => 'GET',
-                'header' => [
-                    'User-Agent: PHP'
-                ]
+    $options = [
+        'http' => [
+            'method' => 'GET',
+            'header' => [
+                'User-Agent: PHP'
             ]
-        ];
-
-        $context = stream_context_create($options);
-        // TODO: erease @
-        $content = @file_get_contents("https://api.github.com/repos/" . $owner . "/" . $repository . "/git/refs/heads/" . $branch . "", false, $context);
-        $decoded = json_decode($content);
-        $message = $decoded->object->sha . "\n";
-    } catch (\Throwable $throwable) {
-        $message = "Something more or less went wrong: \n" . $throwable->getMessage() . "\n";
+        ]
+    ];
+    $context = stream_context_create($options);
+    // @ below -> very, very bad practice, don't try this at home.
+    $content = @file_get_contents("https://api.github.com/repos/" . $owner . "/" . $repository . "/git/refs/heads/" . $branch . "", false, $context);
+    if (false === $content) {
+        return "\n You entered the wrong parameters, check them and try again. \n";
     }
+    $decodedContent = json_decode($content);
 
-    return $message;
+    return $decodedContent->object->sha . "\n";
 }
