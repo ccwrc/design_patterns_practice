df84093b8c1b900241b7dc4d65931f9376d08d9f
ccwrc <ccwrcltd@gmail.com>
2019-09-18 17:26:46 +0200

    GenetorFeature

---
+++ ExperimentalFolder/Php55features/GeneratorFeature.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\ExperimentalFolder\Php55features;
+
+/**
+ * @link https://www.php.net/manual/en/language.generators.overview.php docs.
+ */
+class GeneratorFeature
+{
+    public function __construct()
+    {
+        // create file
+    }
+
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+        // delete file from constructor
+    }
+
+}
