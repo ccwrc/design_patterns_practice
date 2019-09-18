cc732e59cd1ea03446374cb727100b0f29d1f318
ccwrc <ccwrcltd@gmail.com>
2019-09-18 18:13:06 +0200

    gitignore update

--- ExperimentalFolder/Php55features/GeneratorFeature.php
+++ ExperimentalFolder/Php55features/GeneratorFeature.php
@@ -9,15 +9,33 @@ namespace Patterns\ExperimentalFolder\Php55features;
  */
 class GeneratorFeature
 {
+    private static $pathToTestTxtFile = __DIR__ . '/TestGenerator.txt';
+
     public function __construct()
     {
-        // create file
+        self::createTestTxtFile();
+        self::fillUpTestTxtFile(5);
     }
 
     public function __destruct()
     {
         // TODO: Implement __destruct() method.
         // delete file from constructor
+    }
+
+    private static function createTestTxtFile(): void
+    {
+        $file = \fopen(self::$pathToTestTxtFile, 'w');
+        \fclose($file);
+    }
+
+    private static function fillUpTestTxtFile(int $howManyLines): void
+    {
+        $file = \fopen(self::$pathToTestTxtFile, 'a');
+        for ($i = 1; $i <= abs($howManyLines); $i++) {
+            \fwrite($file, 'line ' . $i . "\n");
+        }
+        \fclose($file);
     }
 
 }
---
+++ tests/unit/ExperimentalFolder/Php55features/GeneratorFeatureTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\tests\unit\ExperimentalFolder\Php55features;
+
+use Patterns\ExperimentalFolder\Php55features\GeneratorFeature;
+
+use PHPUnit\Framework\TestCase;
+
+class GeneratorFeatureTest extends TestCase
+{
+    public function testCreate(): void
+    {
+        $generator = new GeneratorFeature();
+        // TODO
+    }
+}
