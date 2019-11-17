a1712cc450042b6b129e797cdf09c5f227f9445f
ccwrc <ccwrcltd@gmail.com>
2019-11-17 17:50:21 +0100

    new test

---
+++ tests/unit/ExperimentalFolder/MicroLoggerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\tests\unit\ExperimentalFolder;
+
+use Patterns\ExperimentalFolder\EnvironmentVariables;
+
+use PHPUnit\Framework\TestCase;
+
+class MicroLoggerTest extends TestCase
+{
+    public function testIsExperimentalFolderIsWritable(): void
+    {
+        EnvironmentVariables::load();
+
+        if ('true' === ($_ENV['TEST_IS_EXPERIMENTAL_FOLDER_IS_WRITABLE'] ?? false)) {
+            $this->assertTrue(\is_writable(\dirname(__DIR__, 3) . '/ExperimentalFolder'));
+        } else if ('true' !== ($_ENV['TEST_IS_EXPERIMENTAL_FOLDER_IS_WRITABLE'] ?? false)) {
+            $this->markTestSkipped('Test disabled in the configuration .env (.env.dist) file.');
+        }
+    }
+}
