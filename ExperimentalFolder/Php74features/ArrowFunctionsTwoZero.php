7ae8229d961d417e5f64e5e8efebb85d47b0a1a2
ccwrc <ccwrcltd@gmail.com>
2019-11-30 15:33:43 +0100

    ArrowFunctionsTwoZero added

---
+++ ExperimentalFolder/Php74features/ArrowFunctionsTwoZero.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\ExperimentalFolder\Php74features;
+
+/**
+ * @link https://wiki.php.net/rfc/arrow_functions_v2 docs
+ */
+final class ArrowFunctionsTwoZero
+{
+    private const WORLD_STAMP = ' Been here. Tony Halik';
+
+    private array $array;
+
+    public function __construct(array $array)
+    {
+        $this->array = $array;
+    }
+
+    public function modifyArrayOldSyntax(): array
+    {
+        return \array_map(function ($probablyLocationInString) {
+            return \is_string($probablyLocationInString) ? $probablyLocationInString . self::WORLD_STAMP : self::WORLD_STAMP;
+        }, $this->array);
+    }
+
+    public function modifyArrayNewSyntax(): array
+    {
+        return \array_map(fn($location) => \is_string($location) ? $location . self::WORLD_STAMP : self::WORLD_STAMP, $this->array);
+    }
+}
---
+++ tests/unit/ExperimentalFolder/Php74features/ArrowFunctionsTwoZeroTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\tests\unit\ExperimentalFolder\Php74features;
+
+use Patterns\ExperimentalFolder\Php74features\ArrowFunctionsTwoZero;
+
+use PHPUnit\Framework\TestCase;
+
+class ArrowFunctionsTwoZeroTest extends TestCase
+{
+    public static function getNonEmptyArray(): array
+    {
+        return [
+            'Where pepper grows. ',
+            'Where vanilla grows. ',
+            'Where almonds bloom. ',
+            'Where eucalyptus smells. '
+        ];
+    }
+
+    public function testArrowFunctionOnArray(): void
+    {
+        $nonEmptyArray = ArrowFunctionsTwoZeroTest::getNonEmptyArray();
+
+        $arrowFunctions = new ArrowFunctionsTwoZero($nonEmptyArray);
+        $oldSyntaxModifiedTable = $arrowFunctions->modifyArrayOldSyntax();
+        $newSyntaxModifiedTable = $arrowFunctions->modifyArrayNewSyntax();
+
+        $this->assertEquals($oldSyntaxModifiedTable, $newSyntaxModifiedTable);
+        $this->assertNotEquals($nonEmptyArray, $newSyntaxModifiedTable);
+    }
+}
