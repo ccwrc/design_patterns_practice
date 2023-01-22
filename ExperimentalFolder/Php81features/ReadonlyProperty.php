c147641814bf411f1f52a610ff33071a8c7669e6
ccwrc <ccwrcltd@gmail.com>
2023-01-22 20:25:34 +0100

    readonly property php8.1

---
+++ ExperimentalFolder/Php81features/ReadonlyProperty.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\ExperimentalFolder\Php81features;
+
+/**
+ * @link https://php.watch/versions/8.1/readonly description
+ * @link https://wiki.php.net/rfc/readonly_properties_v2 official docs
+ */
+final class ReadonlyProperty
+{
+    public const DEFAULT_RENTAL_TIME = 14;
+
+    public readonly string $title;
+    public readonly int $numberOfPages;
+    private readonly bool $isUsed;
+    public int $rentalTime = self::DEFAULT_RENTAL_TIME;
+
+    /**
+     * Book from library for example.
+     */
+    public function __construct(
+        string $title,
+        int    $numberOfPages
+    )
+    {
+        $this->title = $title;
+        $this->numberOfPages = $numberOfPages;
+    }
+
+    public function readBook(): self
+    {
+        return $this;
+    }
+
+    /**
+     * Property $isUsed can only be set once.
+     *
+     * @param true $isUsed
+     *
+     * @return void
+     */
+    public function setIsUsed(true $isUsed): void
+    {
+        $this->isUsed = $isUsed;
+    }
+
+    public function itIsUsed(): bool
+    {
+        return $this->isUsed ?? false;
+    }
+}
---
+++ tests/unit/ExperimentalFolder/Php81features/ReadonlyPropertyTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\tests\unit\ExperimentalFolder\Php81features;
+
+use Patterns\ExperimentalFolder\Php81features\ReadonlyProperty;
+use PHPUnit\Framework\TestCase;
+
+class ReadonlyPropertyTest extends TestCase
+{
+    public function testSetTitle(): void
+    {
+        $book = new ReadonlyProperty(
+            'Fiasko by Stanisław Lem',
+            379
+        );
+
+        $this->expectError();
+        $book->title = 'Failure';
+    }
+
+    public function testIsUsed(): void
+    {
+        $book = new ReadonlyProperty(
+            'Niezwyciężony by Stanisław Lem',
+            260
+        );
+
+        $this->assertFalse($book->itIsUsed());
+
+        $book->setIsUsed(true);
+        $this->assertTrue($book->itIsUsed());
+
+        $this->expectError();
+        $book->setIsUsed(true);
+    }
+
+    public function testRentalTime(): void
+    {
+        $book = new ReadonlyProperty(
+            'Ostatni eksperyment by Julia Iwanowa',
+            126
+        );
+
+        $this->assertEquals($book::DEFAULT_RENTAL_TIME, $book->rentalTime);
+
+        $book->rentalTime = 12;
+        $this->assertEquals(12, $book->rentalTime);
+
+        $book->rentalTime = 77;
+        $this->assertEquals(77, $book->rentalTime);
+    }
+}
