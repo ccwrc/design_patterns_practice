7ef0a6d70e23d3453f08049cfbe015e195ce9980
ccwrc <ccwrcltd@gmail.com>
2023-12-03 18:55:03 +0100

    new contest

---
+++ ExperimentalFolder/Other/AtariChristmasTreeContest2023.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\ExperimentalFolder\Other;
+
+/**
+ * @link https://atarionline.pl/forum/comments.php?DiscussionID=7311 contest
+ */
+final readonly class AtariChristmasTreeContest2023
+{
+    public const string LINK_TO_CONTEST = 'https://atarionline.pl/forum/comments.php?DiscussionID=7311';
+
+    public const string WINNER_PREVIOUS_CONTEST = 'Wolfen';
+
+    public const array OLD_AND_TIRED_PEOPLE = [
+        'string',
+        'gorgh',
+        'pigula',
+        'triplex aka blasph',
+    ];
+}
