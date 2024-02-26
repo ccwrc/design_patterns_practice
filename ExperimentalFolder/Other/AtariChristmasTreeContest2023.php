696ab4c04a1c72504d8468c1d7603773c9a8866f
ccwrc <ccwrcltd@gmail.com>
2024-02-27 00:47:32 +0100

    Atari christmas tree contest 2023 winner

--- ExperimentalFolder/Other/AtariChristmasTreeContest2023.php
+++ ExperimentalFolder/Other/AtariChristmasTreeContest2023.php
@@ -13,10 +13,14 @@ final readonly class AtariChristmasTreeContest2023
 
     public const string WINNER_PREVIOUS_CONTEST = 'Wolfen';
 
+    public const string YOUTUBE_MAIN_CHANNEL = 'https://www.youtube.com/@larek';
+    public const string YOUTUBE_MAIN_RECORD = 'https://www.youtube.com/watch?v=8bu2splNbbg';
+    public const string YOUTUBE_LOTTERY_PART = 'https://youtu.be/8bu2splNbbg?t=16771';
+
     public const array OLD_AND_TIRED_PEOPLE = [
         'string',
         'gorgh',
-        'pigula',
+        'pigula', //winner (24.02.2024)
         'triplex aka blasph',
         'sun',
         'Caesar',
